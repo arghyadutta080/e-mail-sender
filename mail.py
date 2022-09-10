9087eb8863a2abcd553712884ab8f3cb078ca71c
Arghya <arghyadutta080@gmail.com>
2022-09-10 13:04:46 +0530

    first commit

---
+++ mail.py
@@ -0,0 +1,21 @@
+import smtplib
+
+
+def mail_send():
+    my_server = smtplib.SMTP('smtp.gmail.com', 587)
+    my_server.starttls()
+    my_server.login('pythonbot010@gmail.com', 'herduvhpzaungvjj')
+    sender_id = input("Enter sender's id : ")
+    mail_body = input("Enter the message of mail-body : ")
+    my_server.sendmail('pythonbot010@gmail.com',
+                       sender_id,
+                       mail_body)
+    print("! Mail sent successfully !")
+
+
+while (1):
+    confirm = input("Do u want to send an e-mail ? ")
+    if (confirm != "Yes" and confirm != "yes"):
+        break
+    else:
+        mail_send()
